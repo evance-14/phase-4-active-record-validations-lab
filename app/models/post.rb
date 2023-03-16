5186bec053fe5e2b62ff04b54501a2db1d107dc0
evance-14 <kiropevans097@gmail.com>
2023-03-16 15:49:57 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :title_is_clickbait
+
+
+    def title_is_clickbait
+        if !title || (!title.include?("Won't Believe") && !title.include?("Secret") && !title.match?(/Top \d+/) && !title.include?("Guess"))
+          errors.add(:title, "is not sufficiently clickbait-y")
+        end
+      end
 end
